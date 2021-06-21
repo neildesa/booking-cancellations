e59a5d76a2dbcb6f0eab5a2c38ef2824fc52ff6a
neildesa <neilsondesa@gmail.com>
2021-06-21 20:56:46 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting the cancellations of bookings via Linear Regression Models.',
+    author='Neilson Desa',
+    license='MIT',
+)
